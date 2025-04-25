cf17b757fa6f1fb0bed7156dfd21651a035807e8
AarinS-dev <aarin.sandilya12@gmail.com>
2025-04-24 20:23:21 -0500

    Update PetHandler.py

--- PetHandler.py
+++ PetHandler.py
@@ -1,6 +1,32 @@
-class Dog:
-    def __init__(self, name):
+class Pets:
+    def __init__(self, name, species, age):
         self.name = name
+        self.species = species
+        self.age = age
+    def display_info(self):
+        print(f"{self.name} is a {self.species} and is {self.age} years old")
+        
+class PetFunctionality:
+    def __init__(self):
+        self.pets = [
+
+        ]
+    def addPet(self, pet):
+        self.pets.append(pet)
+    
+    def adoptPet(self, name):
+        for peto in self.pets:
+            if peto.name.lower() == name.lower():
+                self.pets.remove(peto)
+                return True
+        print(f"Sorry, {name} is not available unfortunetly")
+        return False
+    def display_all_pets(self):
+        if not self.pets:
+            print("No Pets!")
+        else:
+            for pet in self.pets:
+                pet.display_info()
 
 
 
